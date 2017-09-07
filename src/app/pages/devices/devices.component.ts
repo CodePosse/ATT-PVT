ac43a510468a18db89852d011aa44f5e427e7e13
T. Hunold <codeposse@gmail.com>
2017-09-07 13:16:16 -0700

    continuation of ui files

---
+++ src/app/pages/devices/devices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-devices',
+  templateUrl: './devices.component.html',
+  styleUrls: ['./devices.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DevicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {  }
+
+}
