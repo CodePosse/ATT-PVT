ea8771bc51dcd6aaba6cc00936698d3e08b62412
T. Hunold <codeposse@gmail.com>
2017-10-10 18:51:08 -0700

    charts and wizard flow

--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -6,10 +6,16 @@ import { DirectivesModule } from '../../theme/directives/directives.module';
 import { PieComponent } from './pie/pie.component';
 import { SafePipe } from './../../safe.pipe';
 import { Ng2GoogleChartsModule } from 'ng2-google-charts';
+import { LineComponent } from './line/line.component';
+import { BarComponent } from './bar/bar.component';
+import { BubbleComponent } from './bubble/bubble.component';
 
 export const routes = [
   { path: '', redirectTo: 'pie', pathMatch: 'full'},
-  { path: 'pie', component: PieComponent }
+  { path: 'pie', component: PieComponent },
+  { path: 'bar', component: BarComponent, data: { breadcrumb: 'Bar Charts' } },
+  { path: 'line', component: LineComponent, data: { breadcrumb: 'Line Charts' } },
+  { path: 'bubble', component: BubbleComponent, data: { breadcrumb: 'Bubble Charts' } }
 ];
 
 @NgModule({
@@ -22,7 +28,10 @@ export const routes = [
   ],
   declarations: [
     PieComponent,
-    SafePipe
+    SafePipe,
+    BarComponent,
+    LineComponent,
+    BubbleComponent
   ]
 })
 export class ChartsModule { }
