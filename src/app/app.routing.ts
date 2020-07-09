2e4b55d36aa365ce9ecc8a4bf93696610c2df28c
codeposse <codeposse@gmail.com>
2020-07-08 17:54:12 -0700

    package updates

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,9 +5,9 @@ import { NotFoundComponent } from './pages/errors/not-found/not-found.component'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
-  { path: 'login', loadChildren: 'app/pages/login/login.module#LoginModule' },
-  { path: 'register', loadChildren: 'app/pages/register/register.module#RegisterModule' },
+  { path: 'pages', loadChildren: () => import('app/pages/pages.module').then(m => m.PagesModule) },
+  { path: 'login', loadChildren: () => import('app/pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('app/pages/register/register.module').then(m => m.RegisterModule) },
   { path: '**', component: NotFoundComponent }
 ];
 
